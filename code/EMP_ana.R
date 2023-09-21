0ae94ee1885642910ba0e6556f34b856dced78ba
DaniDuan <d.duan20@imperial.ac.uk>
2023-09-21 16:55:24 +0100

    add in EMP initial filter

---
+++ code/EMP_ana.R
@@ -0,0 +1,20 @@
+
+EMP_data = read.csv("../data/EMP.csv", header = T)
+names(EMP_data) = c("ID", "Sample", "Temp", "pH", "Richness")
+EMP_data = subset(EMP_data,Temp != "" & Temp <= 25 & Temp > 0)
+scatter.smooth(EMP_data$Temp, EMP_data$Richness)
+
+m = max(EMP_data$Richness)
+EMP_data$relative_rich = EMP_data$Richness/m
+scatter.smooth(EMP_data$Temp, EMP_data$relative_rich)
+
+# write.csv(EMP_data, "../data/EMP_filtered.csv", row.names=T)
+
+
+quantile(EMP_data$relative_rich, probs = 0.99)
+
+quan = c()
+for(i in unique(EMP_data$Temp)){
+  sub = subset(EMP_data, Temp == i)
+  quan = c(quan, quantile(sub$relative_rich, probs = 0.99))
+}
